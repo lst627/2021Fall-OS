50c93347db9325011b97815f10d40634020b61f6
lst627 <1776436133@qq.com>
2021-10-28 23:36:46 +0800

    Init

---
+++ Thread-1/lib/model.h
@@ -0,0 +1,19 @@
+#ifndef THREAD_LIB_MODEL_H_
+#define THREAD_LIB_MODEL_H_
+
+#include <vector>
+#include "embedding.h"
+
+namespace proj1 {
+
+double similarity(Embedding* entityA, Embedding* entityB);
+
+EmbeddingGradient* calc_gradient(Embedding* entityA, Embedding* entityB, int label);
+
+EmbeddingGradient* cold_start(Embedding* newUser, Embedding* item);
+
+Embedding* recommend(Embedding* user, std::vector<Embedding*> items);
+
+} // namespace proj1
+
+#endif // THREAD_LIB_MODEL_H_
